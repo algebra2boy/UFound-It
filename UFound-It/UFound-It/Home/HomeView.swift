d6e9a9c769782b269e5791ebbeb3a5f6083ea870
Yongye Tan <yongyetan1209@gmail.com>
2024-11-08 23:40:02 -0500

    add annotation and bottom sheet

--- UFound-It/UFound-It/Home/HomeView.swift
+++ UFound-It/UFound-It/Home/HomeView.swift
@@ -8,20 +8,48 @@
 import SwiftUI
 import MapKit
 
-extension CLLocationCoordinate2D {
-    static let amherst: CLLocationCoordinate2D = .init(latitude: 42.3909, longitude: -72.5257)
-}
-
 struct HomeView: View {
-    
-    @State private var cameraPosition: MapCameraPosition = MapCameraPosition.region(.init(
-        center: .amherst,
+
+    @State private var cameraPosition: MapCameraPosition = .region(.init(
+        center: .ILC,
         span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
     ))
-    
+
+    @State private var presentBottomSheet: Bool = false
+
     var body: some View {
-        Map(position: $cameraPosition)
+        Map(position: $cameraPosition) {
+            Annotation("Box", coordinate: .ILC) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 2)
+                        .fill(.background)
+                    RoundedRectangle(cornerRadius: 2)
+                        .stroke(.secondary, lineWidth: 2)
+                    Image(systemName: "shippingbox")
+                        .padding(5)
+                }
+                .onTapGesture {
+                    presentBottomSheet.toggle()
+                    print("i have been tapped")
+                }
+            }
+            //            .annotationTitles(.hidden)
+
+        }
+        .sheet(isPresented: $presentBottomSheet) {
+            Text("Hello, World!")
+                .presentationDetents([.fraction(0.4)])
+                .presentationDragIndicator(.visible)
+        }
     }
+}
+
+extension CLLocationCoordinate2D {
+    static let ILC: Self = .init(latitude: 42.3909, longitude: -72.5257)
+    static let LGRC: Self = .init(latitude: 42.340382, longitude: -72.496819)
+    static let StudentUnion: Self = .init(latitude: 42.391155, longitude: -72.526711)
+
+    static let coordinates: [Self] = [.LGRC, .StudentUnion]
 }
 
 #Preview {
