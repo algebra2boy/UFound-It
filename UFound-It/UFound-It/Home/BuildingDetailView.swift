2a198d8dcbd3c42717d12dd63b29c7f10d992b19
Yongye Tan <yongyetan1209@gmail.com>
2024-11-09 02:10:45 -0500

    fix individual image view

--- UFound-It/UFound-It/Home/BuildingDetailView.swift
+++ UFound-It/UFound-It/Home/BuildingDetailView.swift
@@ -8,77 +8,81 @@
 import SwiftUI
 
 struct BuildingDetailView: View {
-    
+
     @State private var searchText: String = ""
-    
+
     @State private var columns: [GridItem] = [
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
-    
-    let data = (1...20).map { "Item \($0)" }
-    
+
+    let data = (1...2).map { "Item \($0)" }
+
     var body: some View {
         NavigationStack {
-            
+
             VStack {
-                
+
                 ScrollView {
-                    
-                    LazyVGrid(columns: columns, spacing: 20) {
-                        
+
+                    LazyVGrid(columns: columns, spacing: 30) {
+
                         ForEach(data, id: \.self) { item in
-                            
-                            VStack(alignment: .leading) {
-                                
-                                
-                                asyncUrlImage(url: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Yellow_Transparent_%28cropped%29.jpg")
-                                    .frame(height: 100)
-                                Text(item)
-                                    .padding(.horizontal)
+
+                            VStack(alignment: .leading, spacing: 0) {
+
+                                VStack(alignment: .leading) {
+                                    asyncImage(url: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Yellow_Transparent_%28cropped%29.jpg")
+                                        .frame(height: 200)
+                                }
+                                .cornerRadius(10)
+                                .clipped()
+
+                                HStack {
+                                    Text("Name: \(item)")
+                                        .font(.system(size: 20, weight: .light))
+                                }
+                                .padding(.horizontal, 5)
+
+                                HStack {
+                                    Text("Posted @ 5:00pm")
+                                        .font(.system(size: 20, weight: .light))
+                                }
+                                .padding(.horizontal, 5)
                             }
-                            .background(Color.white)
                             .cornerRadius(10)
-                            .shadow(radius: 2)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color.black, lineWidth: 5)
+                            }
                         }
                     }
-                    .padding()
                 }
+                .padding()
             }
-            
-            .searchable(text: $searchText, prompt: "Search for lost items")
-            .navigationTitle("Franklin dinning hall")
-            .navigationBarTitleDisplayMode(.inline)
         }
-        
+
+        .searchable(text: $searchText, prompt: "Search for lost items")
+        .navigationTitle("Franklin dinning hall")
+        .navigationBarTitleDisplayMode(.inline)
     }
-    
-    
-    
-    @ViewBuilder
-    func asyncUrlImage(url: String) -> some View {
-        if let url = URL(string: url) {
-            AsyncImage(url: url,
-                       content: { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-            },
-                       placeholder: {
-                ProgressView() // loading placeholder
-            })
-            
-        } else {
-            Image(systemName: "photo") // default image
-        }
-    }
-    
-    
+
 }
 
-
-
-
+@ViewBuilder
+func asyncImage(url: String) -> some View {
+    if let url = URL(string: url) {
+        AsyncImage(url: url) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        } placeholder: {
+            ProgressView()
+        }
+    } else {
+        Image(systemName: "photo") // default image
+    }
+}
 
 #Preview {
     BuildingDetailView()
