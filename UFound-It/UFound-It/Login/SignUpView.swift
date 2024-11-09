efded56654fc8ba2ba167259026e617728905152
Chengtao <chengtaolinctl@gmail.com>
2024-11-09 13:07:08 -0500

    Updated: Login & Sign View

--- UFound-It/UFound-It/Login/SignUpView.swift
+++ UFound-It/UFound-It/Login/SignUpView.swift
@@ -1,5 +1,5 @@
 //
-//  LoginView.swift
+//  SignUpView.swift
 //  UFound-It
 //
 //  Created by CHENGTAO LIN on 11/9/24.
@@ -7,51 +7,32 @@
 
 import SwiftUI
 
-struct LoginView: View {
+struct SignUpView: View {
     @State private var username: String = ""
     @State private var password: String = ""
-    @State private var isSigningInView: Bool = false
-    var isSignInButtonDisabled: Bool {
-        [username, password].contains(where: \.isEmpty)
-    }
+    @State private var isSigningInView: Bool = true
     var body: some View {
         ZStack {
             
             Color.gray.opacity(0.15)
-            
             VStack {
                 
                 Spacer()
                 AuthHeaderView()
                     .padding()
-                
                 UserNamePasswordView(
                     username: $username,
                     password: $password,
                     isSignInButton: $isSigningInView)
-                
                 .frame(width: 350)
-                HStack {
-                    Text("Don't have an account? ")
-                        .bold()
-                    NavigationLink(destination: SignUpView()) {
-                        Text("Sign up")
-                            .underline()
-                            .foregroundColor(.blue)
-                    }
-                }
                 Spacer()
-                
                 Spacer()
             }
-            
         }
         .ignoresSafeArea()
-
-    }
-    
+        }
 }
 
 #Preview {
-    LoginView()
+    SignUpView()
 }
