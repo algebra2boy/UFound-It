fe3adb51710c93900e04c885f83105c2289b12d7
Yongye Tan <yongyetan1209@gmail.com>
2024-11-08 23:53:24 -0500

    add franklin coordinate

--- UFound-It/UFound-It/Home/HomeView.swift
+++ UFound-It/UFound-It/Home/HomeView.swift
@@ -9,16 +9,16 @@ import SwiftUI
 import MapKit
 
 struct HomeView: View {
-    
+
     @State private var cameraPosition: MapCameraPosition = .region(.init(
         center: .ILC,
         span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
     ))
-    
-    let coordinates: [CLLocationCoordinate2D] = [.ILC, .ISB]
-    
+
+    let coordinates: [CLLocationCoordinate2D] = [.ILC, .ISB, .Franklin]
+
     @State private var presentBottomSheet: Bool = false
-    
+
     var body: some View {
         Map(position: $cameraPosition) {
             ForEach(coordinates, id: \.self) { coordinate in
@@ -36,7 +36,7 @@ struct HomeView: View {
                     }
                 }
             }
-            
+
         }
         .sheet(isPresented: $presentBottomSheet) {
             Text("Hello, World!")
@@ -51,11 +51,12 @@ extension CLLocationCoordinate2D: @retroactive Hashable {
     static let ILC: Self = .init(latitude: 42.3909, longitude: -72.5257)
     static let LGRC: Self = .init(latitude: 42.340382, longitude: -72.496819)
     static let ISB: Self = .init(latitude: 42.3926, longitude: -72.5250)
-    
+    static let Franklin: Self = .init(latitude: 42.3893, longitude: -72.5225)
+
     public static func ==(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
         return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
     }
-    
+
     public func hash(into hasher: inout Hasher) {
         hasher.combine(latitude)
         hasher.combine(longitude)
