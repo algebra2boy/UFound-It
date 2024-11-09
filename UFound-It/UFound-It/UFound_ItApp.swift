8bcb490cbcc9401c5c2195daf4936deb2582ce4f
Yongye Tan <yongyetan1209@gmail.com>
2024-11-08 22:28:56 -0500

    push xcode project

---
+++ UFound-It/UFound-It/UFound_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  UFound_ItApp.swift
+//  UFound-It
+//
+//  Created by Yongye on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct UFound_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
