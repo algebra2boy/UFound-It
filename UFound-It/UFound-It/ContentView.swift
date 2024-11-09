7b351401352e97d76858668e54b8ec31946abb8a
Yongye Tan <yongyetan1209@gmail.com>
2024-11-08 22:40:41 -0500

    add bottom tabs

--- UFound-It/UFound-It/ContentView.swift
+++ UFound-It/UFound-It/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum AppTab: String, Hashable {
+    case home
+    case profile
+
+    var title: String {
+        switch self {
+        case .home: return "Home"
+        case .profile: return "profile"
         }
-        .padding()
+    }
+
+    var systemImage: String {
+        switch self {
+        case .home: return "mappin.and.ellipse"
+        case .profile: return "person"
+        }
+    }
+}
+
+struct ContentView: View {
+
+    @State private var selectedTab: AppTab = .home
+
+    var body: some View {
+
+        TabView(selection: $selectedTab) {
+            Tab(AppTab.home.title, systemImage: AppTab.home.systemImage, value: AppTab.home) {
+                Text("home")
+            }
+
+            Tab(AppTab.profile.title, systemImage: AppTab.profile.systemImage, value: AppTab.profile) {
+                Text("profile")
+            }
+
+        }
     }
 }
 
