bef9f918c8b377d25059829a3c927535b9c56865
Haijie Qin <jackqin2000@gmail.com>
2024-11-09 20:21:01 -0500

    add ssl

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const fs = require("fs");
+const https = require("https");
+const morgan = require("morgan");
+
 const authRoutes = require("./routes/auth");
 const itemRoutes = require("./routes/items");
-const morgan = require("morgan");
-const locationRoutes = require("./routes/locations")
-const { connect } = require("./config/mongoDB"); // Only importing `connect` to initialize MongoDB connection
+const locationRoutes = require("./routes/locations");
+const { connect } = require("./config/mongoDB");
 const { initWebSocketServer } = require("./utils/websocket");
 
 dotenv.config();
@@ -16,8 +19,8 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use("/uploads", express.static("uploads")); // Serve uploaded images
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -27,18 +30,27 @@ app.use("/api/locations", locationRoutes);
 // MongoDB Connection
 async function initializeMongoDB() {
     try {
-        await connect(); // Establish MongoDB connection on server startup
+        await connect();
         console.log("MongoDB connection initialized.");
     } catch (error) {
         console.error("Error initializing MongoDB connection:", error);
-        process.exit(1); // Exit if MongoDB connection fails
+        process.exit(1);
     }
 }
 
 initializeMongoDB();
 
-// Start Server
-const server = app.listen(process.env.PORT || 3000, () => console.log(`Server running on port ${process.env.PORT || 3000}`));
+// Load SSL certificate and private key
+const sslOptions = {
+    key: fs.readFileSync("certificates/ufound-tech-private.key"),
+    cert: fs.readFileSync("certificates/ufound-tech-certificate.crt"),
+    ca: fs.readFileSync("certificates/ufound-tech-ca_bundle.crt")
+};
+
+// Start HTTPS server
+const server = https.createServer(sslOptions, app).listen(process.env.PORT || 3000, () => {
+    console.log(`HTTPS server running on port ${process.env.PORT || 3000}`);
+});
 
 // Initialize WebSocket Server
 initWebSocketServer(server);
